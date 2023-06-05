23fcc66708f2dfddc43d04309ec0a6eb239ffa4f
Jimmykumar Patel <Jimmykumar.Patel@einfochips.com>
2023-06-05 18:06:00 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './home/products/products.component';
+import { InventoryComponent } from './home/inventory/inventory.component';
+import { SupplierComponent } from './home/supplier/supplier.component';
+import { AddSupplierComponent } from './home/add-supplier/add-supplier.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"products",component:ProductsComponent},
+  {path:"inventory",component:InventoryComponent},
+  {path:"suppliers",component:SupplierComponent},
+  {path:"addsuppliers",component:AddSupplierComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './home/products/products.component';
+import { InventoryComponent } from './home/inventory/inventory.component';
+import { FormsModule } from '@angular/forms';
+import { SupplierComponent } from './home/supplier/supplier.component';
+import { AddSupplierComponent } from './home/add-supplier/add-supplier.component';
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductsComponent,
+    InventoryComponent,
+    SupplierComponent,
+    AddSupplierComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    OAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
